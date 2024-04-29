5ada5bfdcdf0d48670e072aa34168a4b85840c03
Renan.T <rtoniolo4@gmail.com>
2024-04-29 20:47:41 -0300

    build: :wrench: config docker and sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { McsModule } from './mcs/mcs.module';
+import sequelizeConfig from './config/ormconfig';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'lelouch',
-      password: '123456',
-      database: 'mydatabase',
-      models: [],
-    }),
-  ],
+  imports: [SequelizeModule.forRoot(sequelizeConfig), McsModule],
   controllers: [AppController],
   providers: [AppService],
 })
