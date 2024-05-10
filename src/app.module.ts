4e139fb0d653d4032d2d4c93543c703f5a8f9980
Renan.T <rtoniolo4@gmail.com>
2024-05-10 11:36:42 -0300

    feat: :sparkles: add nest schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
-
 import { SequelizeModule } from '@nestjs/sequelize';
 import { McsModule } from './mcs/mcs.module';
 import { VideosModule } from './videos/videos.module';
 import { BattlesModule } from './battles/battles.module';
 import sequelizeConfig from '../config/ormconfig';
 import { McVideoModule } from './ associative/mcVideo.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksModule } from './tasks-cron/tasks.module';
 
 @Module({
   imports: [
     SequelizeModule.forRoot(sequelizeConfig),
+    ScheduleModule.forRoot(),
+    TasksModule,
     McsModule,
     VideosModule,
     BattlesModule,
